39ca69bc3d16d720ea21d9200c643de8bb3d224a
alvinwalker314 <alvinwalker314@gmail.com>
2019-11-13 19:30:06 -0500

    Finished Lru cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -7,7 +7,11 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.size = 0
+        self.storage = {}
+        self.keys = []
+
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +21,13 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key in self.keys:
+            index = self.keys.index(key)
+            self.keys.pop(index)
+            self.keys.append(key)
+            return self.storage[key]
+        else: 
+           return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +40,42 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        entry = {key: value}
+        # if the key is in list of keys
+        if key in self.keys:
+            # grabbing index of the key
+            index = self.keys.index(key)
+            # Popping key from list
+            self.keys.pop(index)
+            # Placing it back
+            self.keys.append(key)
+            # Deleting key, value from storage
+            del self.storage[key]
+            # adding key, value back to storage
+            self.storage.update(entry)
+        # Runs if key isn't in list of keys
+        else:
+            # Runs if size has reached limit
+            if self.size == self.limit:
+                # Grab first key in key list
+                old_key = self.keys[0]
+                # Delete it from storage
+                del self.storage[old_key]
+                # Delete it from List of keys
+                self.keys.pop(0)
+                # Add entry into storage
+                self.storage.update(entry)
+                # Add key into key list
+                self.keys.append(key)
+            # Runs if limit isn't reached
+            else:
+                # Updates storage with new entry
+                self.storage.update(entry)
+                # updates key list with new key
+                self.keys.append(key)
+                self.size += 1
+
+
+                
+                
+
